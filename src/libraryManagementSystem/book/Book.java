2332699ed73991a1ee0f0c855cc2a0110ffc3614
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-02-05 18:13:55 -0600

    Initalized private attributes for Book

--- src/libraryManagementSystem/book/Book.java
+++ src/libraryManagementSystem/book/Book.java
@@ -1,5 +1,16 @@
 package libraryManagementSystem.book;
 
 public class Book {
-
+	
+	/**
+	 * Initializing the four attributes title, author, ISBN
+	 * and price.
+	 */
+	private String title;
+	private String author;
+	private String ISBN;
+	private double price;
+	
+	
+	
 }
