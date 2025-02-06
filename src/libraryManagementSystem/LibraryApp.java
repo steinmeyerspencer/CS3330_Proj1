3ee29276f16a138cbbd5f0e56148fdb5e4816f20
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-02-05 18:52:04 -0600

    Finished off the book, I think, and did some testing

--- src/libraryManagementSystem/LibraryApp.java
+++ src/libraryManagementSystem/LibraryApp.java
@@ -1,10 +1,29 @@
 package libraryManagementSystem;
 
+import libraryManagementSystem.book.Book;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Book harryPotter = new Book("Harry Potter", "JK Rowling", "1", 20.0);
+		
+		System.out.println("Book title: " + harryPotter.getTitle());
+		
+//		Book lordOfTheRings = new Book("Lord of the Rings", "Tolkien", "1", 30.0);
+		
+		Book harryPotter2 = new Book(harryPotter);
+		
+		System.out.println(harryPotter.toString());
+		
+		
+		if(harryPotter.equals(harryPotter2)) {
+			System.out.println("You have two same books.");
+		}
+		else {
+			System.out.println("You messed up");
+		}
+		
 	}
 
 }
